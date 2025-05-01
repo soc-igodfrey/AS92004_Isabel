0a77cdc866e0f371e6f66736d403a53e904048f3
soc-igodfrey <igodfrey@storans.school.nz>
2025-05-01 15:05:13 +1200

    Class update
    I added more of the answers, set up some variables and some constants

--- main.py
+++ main.py
@@ -19,17 +19,40 @@ questions = (" Jakob Nielsen's 10 Usability Heuristics are best thought of as",
               "Letting users undo things they've done is important for:",
               )
 
-options = (("A. Strict rules for how to code websites and apps.", "B. General tips for making things easy to use.
+options = (("A. Strict rules for how to code websites and apps.", "B. General tips for making things easy to use.",
              "C. Things every app must have to be official.", "D. A list of all the cool features you can add."),
-            "A. Being the same and working the same way everywhere.", "B. Showing you what's happening right now.", "C. Stopping you from making mistakes.", "D. Having help if you need it."),
-            "A. Using complicated computer words in error messages", "B. Putting information in an order that makes sense like in real life.", "C. Having lots of extra information to explain everything", "D. Letting you change how the screen looks with different colours."),
-            "A.", "B", "C", "D"),
-            "A.", "B", "C", "D"),
-            "A.", "B", "C", "D"),
-            "A.", "B", "C", "D"),
-            "A.", "B", "C", "D"),
-            "A.", "B", "C", "D"),
-            "A.", "B", "C", "D"),
-          ))
+            ("A. Being the same and working the same way everywhere.", "B. Showing you what's happening right now.", "C. Stopping you from making mistakes.", "D. Having help if you need it."),
+            ("A. Using complicated computer words in error messages", "B. Putting information in an order that makes sense like in real life.", "C. Having lots of extra information to explain everything", "D. Letting you change how the screen looks with different colours."),
+            ("A. 'Algorithms' making the program run fast", "B.'Looks good and keeps it simple'.", "C. 'Able to be used by everyone'.", "D. 'Things you can click on to do stuff'."),
+           ( "A. Tell you clearly when you've done something wrong.", "B. Help you fix your mistakes quickly.", "C. Stop you from making mistakes in the first place.", "D. Give you lots of instructions so you don't mess up."),
+            ("A.  Being the same and working the same way everywhere.", "B. Being able to do things in different ways and quickly.", "C. Remembering things for you instead of making you remember.", "D. Having help if you need it."),
+            ("A. Showing you what's happening right now.", "B. Looking like things in the real world.", "C. Being the same and working the same way everywhere.", "D. Stopping you from making mistakes."),
+           ("A. Being the same and working the same way everywhere.", "B. Letting you control things and undo mistakes.", "C. Showing you options so you don't have to remember.", "D. Having a simple and good-looking design."),
+            ("A. Just tell you that something went wrong.", "B.  Let you go back and try again.", "C.  Tell you what the problem is and how to fix it.", "D. Stop you from making any more mistakes."),
+            ("A. Showing you what's happening right now.", "B. Letting you be in charge and fix mistakes.", "C. Stopping you from making mistakes.", "D. Having a simple and good-looking design."),
+          )
 
-correct_answer = ["b", "", "", "", "", "", "", "", "", "", ]
+correct_answer = ["b", "b", "b", "b", "c", "b", "c", "", "", "", ]
+
+#Set Up variables
+
+score = 0
+
+#user input variables
+
+name = input("What is your name?")
+
+yes_no = input("Hello Would you like to play a game")
+
+yes_no = yes_no.lower
+
+yes_no = yes_no.strip(1)
+
+no_questions = int(input("How many questions would you like? Please pick between 5-10 questions")
+
+#set up constant
+MIN = 5
+
+MAX = 10
+
+
